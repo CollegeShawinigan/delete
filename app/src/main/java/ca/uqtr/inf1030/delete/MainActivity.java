d53be58870dea7664be95756f9fc949c00e77331
Marco Guilmette <marco.guilmette@infologique.ca>
2017-02-25 19:28:13 -0500

    Initial commit

---
+++ app/src/main/java/ca/uqtr/inf1030/delete/MainActivity.java
@@ -0,0 +1,13 @@
+package ca.uqtr.inf1030.delete;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
